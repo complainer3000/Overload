01726499ca4abd2db54497e16ad2fce1c03fbc38
dwqdqwd <complaining@lmbao.lol>
2024-10-29 17:09:13 +0100

    csgo never runs on 64bit

--- Source/Platform/Macros/IsPlatform.h
+++ Source/Platform/Macros/IsPlatform.h
@@ -4,28 +4,10 @@
 #error "Conflicting platform macros defined"
 #endif
 
-#if defined(_WIN64)
-
-#define IS_WIN32() false
-#define IS_WIN64() true
-#define IS_LINUX() false
-
-#elif defined(_WIN32)
-
-#define IS_WIN32() true
-#define IS_WIN64() false
-#define IS_LINUX() false
-
+#if defined(_WIN32)
+#define IS_WIN32 true
+#define IS_LINUX false
 #elif defined(__linux__)
-
-#define IS_WIN32() false
-#define IS_WIN64() false
-#define IS_LINUX() true
-
-#else
-
-#define IS_WIN32() false
-#define IS_WIN64() false
-#define IS_LINUX() false
-
+#define IS_WIN32 false
+#define IS_LINUX true
 #endif
